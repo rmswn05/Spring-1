f5065e93fcd064c6e13ed5e45eb2e9e8d6a11dd0
John Kang <hphk.john@gmail.com>
2021-04-26 16:36:25 +0900

    Initial Upload

---
+++ bootJsp/src/main/java/com/multi/bootJsp/BootJspApplication.java
@@ -0,0 +1,13 @@
+package com.multi.bootJsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJspApplication.class, args);
+	}
+
+}
